a26dd2107443162770dd196dd95f98f8eee25406
𐰀𐰞𐰃:𐰺𐰃𐰔𐰀:𐰚𐰀𐰾𐰚𐰃𐰤 (𐰽𐰆𐰞𐰃𐰤𐰆𐰽) <parduscix@yandex.ru>
2022-11-10 20:04:13 +0000

    Update path.sh

--- rootfs/etc/profile.d/path.sh
+++ rootfs/etc/profile.d/path.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # local path
 export PATH="/usr/local/sbin:/usr/local/bin:/usr/bin:/bin"
-if [ $UID -eq 0 ] ; then
+if [ $(id -u) -eq 0 ] ; then
     # sbin directory (root only)
     export PATH="$PATH:/usr/sbin:/sbin"
 fi
