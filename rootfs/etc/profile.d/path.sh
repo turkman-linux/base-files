d721d2a6fd1dfaf67174ea9c1006cf6dbdc643ec
𐰀𐰞𐰃:𐰺𐰃𐰔𐰀:𐰚𐰀𐰾𐰚𐰃𐰤 (𐰽𐰆𐰞𐰃𐰤𐰆𐰽) <parduscix@yandex.ru>
2023-02-08 09:35:39 +0000

    Update path.sh

--- rootfs/etc/profile.d/path.sh
+++ rootfs/etc/profile.d/path.sh
@@ -3,7 +3,7 @@
 export PATH="/usr/local/sbin:/usr/local/bin:/usr/bin:/bin"
 if [ $(id -u) -eq 0 ] ; then
     # sbin directory (root only)
-    export PATH="$PATH:/usr/sbin:/sbin"
+    export PATH="/usr/sbin:/sbin:$PATH"
 fi
 # user path
 if [ -d $HOME/bin ] ; then
